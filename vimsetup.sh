de0abcb805a2e581540f4cab43ea59c737ed0e2d
Frederick Pearson <fredp613@gmail.com>
2017-02-16 22:08:39 -0500

    Update vimsetup.sh

--- vimsetup.sh
+++ vimsetup.sh
@@ -5,6 +5,10 @@ repo_nerdtree="https://github.com/scrooloose/nerdtree.git"
 repo_ctrlp="https://github.com/kien/ctrlp.vim.git"
 repo_surroundvim="https://github.com/tpope/vim-surround.git"
 
+mkdir .vim && cd .vim && mkdir bundle && cd
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 cd ~/.vim/bundle/
 
 git clone $repo_nerdtree nerdtree
